a70dd3216b5489a8c46a03448b92d5854c1369fb
algreen11 <AlexT@Alexs-MacBook-Pro-3.local>
2020-06-27 13:38:27 -0400

    format

--- blackjack.py
+++ blackjack.py
@@ -3,66 +3,71 @@ import numpy as np
 import pandas as pd
 import multiprocessing
 
-# matrices that show moves based on cards: 0 = stick, 1 = hit, 2 = doubledown, 3 = split
+# matrices that show moves based on cards: 0 = stick, 1 = hit, 2 =
+# doubledown, 3 = split
 
-# hard total matrix, top column is dealer card, rows denoted by total of user hand
+# hard total matrix, top column is dealer card, rows denoted by total of
+# user hand
 data = np.array([['', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'],
-                ['4', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['5', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['6', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['7', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['8', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['9', 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['10', 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1],
-                ['11', 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-                ['12', 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['13', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['14', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['15', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['16', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]])
-hardTotal = pd.DataFrame(data=data[1:, 1:],
-                   index=data[1:, 0],
-                   columns=data[0, 1:])
+                 ['4', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['5', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['6', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['7', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['8', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['9', 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['10', 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1],
+                 ['11', 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+                 ['12', 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['13', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['14', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['15', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['16', 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]])
+hard_total = pd.DataFrame(data=data[1:, 1:],
+                          index=data[1:, 0],
+                          columns=data[0, 1:])
 
-# soft total matrix 
+# soft total matrix
 data = np.array([['', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'],
-                ['12', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # remove this line when add split logic
-                ['13', 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['14', 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['15', 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['16', 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['17', 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['18', 0, 2, 2, 2, 2, 0, 0, 1, 1, 1, 1, 1, 1],
-                ['19', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                ['20', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                ['21', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-softTotal = pd.DataFrame(data=data[1:, 1:],
-                   index=data[1:, 0],
-                   columns=data[0, 1:])
+                 # remove this line when add split logic
+                 ['12', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['13', 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['14', 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['15', 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['16', 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['17', 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['18', 0, 2, 2, 2, 2, 0, 0, 1, 1, 1, 1, 1, 1],
+                 ['19', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 ['20', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 ['21', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+soft_total = pd.DataFrame(data=data[1:, 1:],
+                          index=data[1:, 0],
+                          columns=data[0, 1:])
 
-# split matrix 
+# split matrix
 data = np.array([['', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'],
-                ['4', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],  # remove this line when add split logic
-                ['6', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
-                ['8', 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['10', 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1],
-                ['12', 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1],
-                ['14', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
-                ['16', 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-                ['18', 3, 3, 3, 3, 3, 0, 3, 3, 0, 0, 0, 0, 0],
-                ['20', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+                 # remove this line when add split logic
+                 ['4', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
+                 ['6', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
+                 ['8', 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['10', 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1],
+                 ['12', 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+                 ['14', 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
+                 ['16', 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+                 ['18', 3, 3, 3, 3, 3, 0, 3, 3, 0, 0, 0, 0, 0],
+                 ['20', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 split = pd.DataFrame(data=data[1:, 1:],
-                   index=data[1:, 0],
-                   columns=data[0, 1:])
+                     index=data[1:, 0],
+                     columns=data[0, 1:])
+
 
 class BlackjackSim:
 
     def __init__(self):
-        #Counting cards: keep running count of high/low cards seen
+        # Counting cards: keep running count of high/low cards seen
         self.count = 0
 
-    def shuffleDeck(self):  # d - diamonds, h - hearts, s - spades, c - clubs
-        deck = [[2, 'd'], [3, 'd'], [4, 'd'], [5, 'd'], [6, 'd'], [7, 'd'], [8, 'd'], [9, 'd'], [10, 'd'], [11, 'd'], [12, 'd'], [13, 'd'], [14, 'd'], 
+    def shuffle_deck(self):  # d - diamonds, h - hearts, s - spades, c - clubs
+        deck = [[2, 'd'], [3, 'd'], [4, 'd'], [5, 'd'], [6, 'd'], [7, 'd'], [8, 'd'], [9, 'd'], [10, 'd'], [11, 'd'], [12, 'd'], [13, 'd'], [14, 'd'],
                 [2, 'h'], [3, 'h'], [4, 'h'], [5, 'h'], [6, 'h'], [7, 'h'], [8, 'h'], [9, 'h'], [10, 'h'], [11, 'h'], [12, 'h'], [13, 'h'], [14, 'h'],
                 [2, 's'], [3, 's'], [4, 's'], [5, 's'], [6, 's'], [7, 's'], [8, 's'], [9, 's'], [10, 's'], [11, 's'], [12, 's'], [13, 's'], [14, 's'],
                 [2, 'c'], [3, 'c'], [4, 'c'], [5, 'c'], [6, 'c'], [7, 'c'], [8, 'c'], [9, 'c'], [10, 'c'], [11, 'c'], [12, 'c'], [13, 'c'], [14, 'c'],
@@ -74,73 +79,76 @@ class BlackjackSim:
 
         return shuffled[:]
 
+    def play_hand(self, deck):
 
-    def playHand(self, deck):
-
-        dealerHand = []
-        userHand = []
-        doubledDown = False
+        dealer_hand = []
+        user_hand = []
+        doubled_down = False
         split = False
 
-        #print("Count is:", getCount())
-        bet = self.placeBet()
+        bet = self.place_bet()
 
         # give cards to player and dealer
         for x in range(4):
             card = deck.pop()
-            self.changeCount(card)
+            self.change_count(card)
             if x % 2 == 0:
-                userHand.append(card)
+                user_hand.append(card)
             else:
-                dealerHand.append(card)
+                dealer_hand.append(card)
 
-        # hit or stick for both player and dealer, don't do anything with blackjack
-        if not ((self.total(userHand) == 21 and self.total(dealerHand) != 21)):
-            doubledDown, split = self.hitOrStickPlayer(deck, userHand, dealerHand[1])
-            if self.total(userHand) < 22 and split is False :  # if split don't deal to dealer yet
-                self.hitOrStickDealer(deck, dealerHand)
+        # hit or stick for both player and dealer, don't do anything with
+        # blackjack
+        if not ((self.total(user_hand) == 21 and self.total(dealer_hand) != 21)):
+            doubled_down, split = self.hit_or_stick_player(
+                deck, user_hand, dealer_hand[1])
+            # if split don't deal to dealer yet
+            if self.total(user_hand) < 22 and split is False:
+                self.hit_or_stick_dealer(deck, dealer_hand)
 
-        # print("user hand:", total(userHand), userHand)
-        # print("dealer hand:", total(dealerHand), dealerHand)
-        # print(doubledDown, split)
         if split is False:
-            return self.scoreHand(userHand, dealerHand, doubledDown, bet)
+            return self.score_hand(user_hand, dealer_hand, doubled_down, bet)
         else:
             winnings, losses, draws = 0, 0, 0
             hands = []
-            for card in userHand:
+            for card in user_hand:
                 hand = [card, deck.pop()]
-                doubledDown, split = self.hitOrStickPlayer(deck, hand, dealerHand[1])  # allows for one extra split 
+                doubled_down, split = self.hit_or_stick_player(
+                    deck, hand, dealer_hand[1])  # allows for one extra split
                 if split:
                     for crd in hand:
                         hnd = [crd, deck.pop()]
-                        doubledDown, split = self.hitOrStickPlayer(deck, hnd, dealerHand[1])  # should make it so that player doesn't have option to split again 
-                        hands.append([hnd, doubledDown])
-                else: 
-                    hands.append([hand, doubledDown])
+                        # should make it so that player doesn't have option to
+                        # split again
+                        doubled_down, split = self.hit_or_stick_player(
+                            deck, hnd, dealer_hand[1])
+                        hands.append([hnd, doubled_down])
+                else:
+                    hands.append([hand, doubled_down])
 
-            self.hitOrStickDealer(deck, dealerHand)
+            self.hit_or_stick_dealer(deck, dealer_hand)
             for hand in hands:
-                w, d, l = self.scoreHand(hand[0], dealerHand, hand[1], bet)
-                if w == 1.5: w = 1  # don't get blackjack bonus after splitting
+                w, d, l = self.score_hand(hand[0], dealer_hand, hand[1], bet)
+                if w == 1.5:
+                    w = 1  # don't get blackjack bonus after splitting
                 winnings += w
                 losses += l
                 draws += d
 
             return winnings, draws, losses
 
-    def scoreHand(self, userHand, dealerHand, doubledDown, bet):
+    def score_hand(self, user_hand, dealer_hand, doubled_down, bet):
         winnings = 0
         draws = 0
         losses = 0
 
-        user = self.total(userHand)
-        dealer = self.total(dealerHand)
+        user = self.total(user_hand)
+        dealer = self.total(dealer_hand)
 
         if user > 21:
             losses = 1
         elif user > dealer or dealer > 21:
-            if len(userHand) == 2 and user == 21:  # blackjack
+            if len(user_hand) == 2 and user == 21:  # blackjack
                 winnings += 1.5
             else:
                 winnings += 1
@@ -149,32 +157,30 @@ class BlackjackSim:
         else:
             draws += 1
 
-        if doubledDown is True and draws < 1:
+        if doubled_down is True and draws < 1:
             if winnings == 0:
                 losses = 2
             else:
                 winnings = 2
 
-        return winnings*bet, draws*bet, losses*bet
+        return winnings * bet, draws * bet, losses * bet
 
-
-    def hitOrStickDealer(self, deck, hand):
+    def hit_or_stick_dealer(self, deck, hand):
         tot = self.total(hand)
         while tot < 17 and not tot > 22:
             card = deck.pop()
-            self.changeCount(card)
+            self.change_count(card)
             hand.append(card)
             tot = self.total(hand)
 
-
     # returns true if doubled down
-    def hitOrStickPlayer(self, deck, hand, dealerCard):
+    def hit_or_stick_player(self, deck, hand, dealer_card):
         tot = self.total(hand)
         while True:
-            move = self.hitLogic(deck, hand, tot, dealerCard)
+            move = self.hit_logic(deck, hand, tot, dealer_card)
             if move == 1:
                 card = deck.pop()
-                self.changeCount(card)
+                self.change_count(card)
                 hand.append(card)
                 tot = self.total(hand)
             elif move == 2:
@@ -184,40 +190,39 @@ class BlackjackSim:
             else:
                 return False, False
 
-
-    def changeCount(self, card):
+    def change_count(self, card):
         if card[0] < 7:
             self.count -= 1
         elif card[0] > 9:
             self.count += 1
 
-
-    def getCount(self):
+    def get_count(self):
         return self.count
 
-    def placeBet(self):
-    	trueCount = int(self.getCount()/ 6)
-    	return max(5, 5 + 5*trueCount)
+    def place_bet(self):
+        trueCount = int(self.get_count() / 6)
+        return max(5, 5 + 5 * trueCount)
 
-
-    def hitLogic(self, deck, hand, total, dealerCard):
+    def hit_logic(self, deck, hand, total, dealer_card):
         soft = [card for card in hand if card[0] == 14]
         if len(hand) == 2 and len(soft) == 1:  # have a soft total
-            move = int(softTotal.loc[str(total)][str(dealerCard[0])])
+            move = int(soft_total.loc[str(total)][str(dealer_card[0])])
         elif len(hand) == 2 and hand[0][0] == hand[1][0]:  # split
-            if hand[0][0] == 14:  # if 2 aces always split - total function doesn't pick up diff between A,A and 6,6
+            # if 2 aces always split - total function doesn't pick up diff
+            # between A,A and 6,6
+            if hand[0][0] == 14:
                 move = 3
             else:
-                move = int(split.loc[str(total)][str(dealerCard[0])])
+                move = int(split.loc[str(total)][str(dealer_card[0])])
         elif total > 16:  # stick
             return 0
         else:  # hard total
-            move = int(hardTotal.loc[str(total)][str(dealerCard[0])]) 
+            move = int(hard_total.loc[str(total)][str(dealer_card[0])])
 
         if move == 1:  # hit
             return 1
         elif move == 2:  # doubledown
-            if len(hand) == 2: 
+            if len(hand) == 2:
                 card = deck.pop()
                 hand.append(card)
                 return 2
@@ -228,10 +233,9 @@ class BlackjackSim:
         else:  # stand
             return 0
 
-
     def total(self, hand):
         tot = 0
-        isAce = False
+        is_ace = False
         for x in hand:
             num = x[0]
             if num > 10 and num < 14:
@@ -239,11 +243,11 @@ class BlackjackSim:
             elif num == 14:
                 if tot <= 10:
                     num = 11
-                    isAce = True
+                    is_ace = True
                 else:
                     num = 1
             tot += num
-        if isAce and tot > 21:
+        if is_ace and tot > 21:
             tot = 0
             for x in hand:
                 num = x[0]
@@ -254,37 +258,37 @@ class BlackjackSim:
                 tot += num
         return tot
 
-
-    def blackJack(self):
+    def black_jack(self):
         winnings = 0
         draws = 0
         losses = 0
         hands = 0
         while hands < 100000:
-            deck = self.shuffleDeck()
+            deck = self.shuffle_deck()
 
             while len(deck) > 78:  # 75% of deck played
-                win, draw, loss = self.playHand(deck)
+                win, draw, loss = self.play_hand(deck)
                 winnings += win
                 draws += draw
                 hands += 1
                 losses += loss
-        
+
         return winnings, losses, draws, hands
 
 
-def classWrapper():
+def class_wrapper():
     game = BlackjackSim()
-    return game.blackJack()
+    return game.black_jack()
 
 
 if __name__ == '__main__':
     # multiply by 100k to get amount of hands to play
-    amountToRun = 8
+    amount_to_run = 8
 
     # execute blackjack function in parallel
     pool = multiprocessing.Pool()
-    results = pool.starmap_async(classWrapper, [() for _ in range(amountToRun)])
+    results = pool.starmap_async(
+        class_wrapper, [() for _ in range(amount_to_run)])
     pool.close()
     pool.join()
 
@@ -296,5 +300,5 @@ if __name__ == '__main__':
     draws = results[2]
     hands = results[3]
 
-    print("Won $%d and lost $%d over %d hands for percentage winnings of %f" % (winnings, losses, hands, float(winnings-losses)/hands))
-
+    print("Won $%d and lost $%d over %d hands for percentage winnings of %f" %
+          (winnings, losses, hands, float(winnings - losses) / hands))
